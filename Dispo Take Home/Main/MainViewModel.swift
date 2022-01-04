b4b4887d786b6192259bf9bbc852f95180efee2a
= <samgusaapp1993@gmail.com>
2022-01-04 17:50:46 -0500

    Changed some data for the mainViewModel. created a response of  a publisher of [SearchResults] and Never, so help show the main data that will appear in collection view.

--- Dispo Take Home/Main/MainViewModel.swift
+++ Dispo Take Home/Main/MainViewModel.swift
@@ -2,32 +2,39 @@ import Combine
 import UIKit
 
 func mainViewModel(
-  cellTapped: AnyPublisher<SearchResult, Never>,
-  searchText: AnyPublisher<String, Never>,
-  viewWillAppear: AnyPublisher<Void, Never>
+    cellTapped: AnyPublisher<SearchResult, Never>,
+    searchText: AnyPublisher<String, Never>,
+    viewWillAppear: AnyPublisher<Void, Never>
 ) -> (
-  loadResults: AnyPublisher<[SearchResult], Never>,
-  pushDetailView: AnyPublisher<SearchResult, Never>
+    loadResults: AnyPublisher<[SearchResult], Never>,
+    pushDetailView: AnyPublisher<SearchResult, Never>,
+    showMainPosts: AnyPublisher<[SearchResult], Never>
 ) {
-  let api = GifAPIClient.live
-
-
+    let api = GifAPIClient.live
     
-  let trendingGifs = Empty<[SearchResult], Never>()
-
-  let searchResults = searchText
-    .map { api.searchGIFs($0) }
-    .switchToLatest()
-
-  // show featured gifs when there is no search query, otherwise show search results
-  let loadResults = trendingGifs
-    .eraseToAnyPublisher()
-
-  let pushDetailView = Empty<SearchResult, Never>()
-    .eraseToAnyPublisher()
-
-  return (
-    loadResults: loadResults,
-    pushDetailView: pushDetailView
-  )
+    let searchResults = searchText
+        .map { api.searchGIFs($0) }
+        .switchToLatest()
+    
+    //main results that appear.
+    let mainResults = api.featuredGIFs
+    
+    
+    // show featured gifs when there is no search query, otherwise show search results
+    let loadResults = searchResults
+        .eraseToAnyPublisher()
+    
+    //show main results
+    let showMainResults = mainResults()
+        .eraseToAnyPublisher()
+    
+    //renamed pushDetailView. What happens when cell pressed
+    let cellPushed = cellTapped
+        .eraseToAnyPublisher()
+    
+    return (
+        loadResults: loadResults,
+        pushDetailView: cellPushed,
+        showMainPosts: showMainResults
+    )
 }
